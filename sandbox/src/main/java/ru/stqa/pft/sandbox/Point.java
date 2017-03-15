7f94d5d683e48629794b8258b378da0f5b29e3b9
sergeiden <sd100578@yandex.ru>
2017-03-15 18:52:52 +0300

    Добавлен метод в классе Point
    Добавлено вычисление  расстояния между двумя точками через метод в
    классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,4 +7,14 @@ public class Point {
 
   public double x;
   public double y;
+
+  public Point (double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+  public double distance(Point p2) {
+    double dx = p2.x - x;
+    double dy = p2.y - y;
+    return Math.sqrt(dx*dx+dy*dy);
+  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/TwoPointsDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/TwoPointsDistance.java
@@ -6,13 +6,13 @@ package ru.stqa.pft.sandbox;
 public class TwoPointsDistance {
 
   public static void main(String[] args) {
-    Point p1 = new Point();
-    p1.x = 2;
-    p1.y = 2;
-    Point p2 = new Point();
-    p2.x = 3;
-    p2.y = 3;
-    System.out.println("Расстояние между точками p1 и p2 равно " + distance (p1, p2));
+    Point p1 = new Point(2,2);
+    Point p2 = new Point(3,3);
+
+    System.out.println("Расстояние между точками p1 и p2 равно " + distance (p1, p2) + " - вычисление через функцию");
+
+    System.out.println("Расстояние между точками p1 и p2 равно " + p1.distance(p2) + " - вычисление через метод");
+
   }
 
   public static double distance(Point p1, Point p2) {
@@ -20,4 +20,5 @@ public class TwoPointsDistance {
     double dy = p2.y - p1.y;
     return Math.sqrt(dx*dx+dy*dy);
   }
+
 }
