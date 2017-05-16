0a6a2bae5b7ed52fdddc336c499f9fbd324fcba2
sergeiden <sd100578@yandex.ru>
2017-05-16 15:22:28 +0300

    Добавлено получение системного свойства target

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -38,7 +38,7 @@ public class ApplicationManager {
   }
 
   public void init() throws IOException {
-  String target = properties.getProperty("target", "local");
+    String target = System.getProperty("target", "local");
 
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
